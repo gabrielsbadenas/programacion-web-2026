b8bc57c84e896ffbc3d80afda680cb5828974789
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2026-03-18 23:10:37 +0000

    ej

--- clase4/unidad1.2.js
+++ clase4/unidad1.2.js
@@ -1,4 +1,7 @@
 let lado= prompt("ingrese el lado de un cuadrado")
+while(lado<=0||isNaN(lado)){
+    lado=prompt("ingrese el lado del cuadrado. numero positivo")
+}
 let perimetro=lado*4
 let area=lado*lado
 alert("El perimetro del cuadrado es: "+perimetro)
--- clase5/notas.js
+++ clase5/notas.js
@@ -1,5 +1,8 @@
 let cantidad = Number(prompt("ingrese cuantas notas")),
 total=0,i=1
+while(cantidad<=0||isNaN(cantidad)){
+    cantidad=Number(prompt("ingrese cuantas notas. numero positivo."))
+}
 for(;i<=cantidad;i++){
     let nota = Number(prompt(`nota ${i}`))
     while(nota>10||nota<1||isNaN(nota)){

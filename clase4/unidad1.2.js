498ccca146f33e264b22c13699a68dd092bb6e3c
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2026-03-18 22:29:37 +0000

    ejercicios

---
+++ clase4/unidad1.2.js
@@ -0,0 +1,5 @@
+let lado= prompt("ingrese el lado de un cuadrado")
+let perimetro=lado*4
+let area=lado*lado
+alert("El perimetro del cuadrado es: "+perimetro)
+alert("El area del cuadrado es: "+area)
---
+++ clase5/notas.js
@@ -0,0 +1,12 @@
+let cantidad = Number(prompt("ingrese cuantas notas")),
+total=0,i=1
+for(;i<=cantidad;i++){
+    let nota = Number(prompt(`nota ${i}`))
+    while(nota>10||nota<1){
+        
+        nota=Number(prompt(`ingrese la nota n°${i}. nota ingresada incorrectamente`))
+    }
+    total+=nota
+}
+let promedio=total/cantidad
+console.log(`tu total es ${total} y tu promedio es ${promedio}`)

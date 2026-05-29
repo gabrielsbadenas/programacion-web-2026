4ecee9cfa867aafdef19289828cf53429252df58
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2026-05-29 23:05:42 +0000

    else

---
+++ 29-5/modoOscuro.js
@@ -0,0 +1,19 @@
+let btnModoOscuro = document.getElementById("btnModoOscuro");
+
+let dark = JSON.parse(localStorage.getItem("dark"));
+if (dark === null) {
+  dark = false;
+} else {
+  document.body.classList.toggle("modoOscuro");
+  btnModoOscuro.innerText = "Light";
+}
+btnModoOscuro.onclick = () => {
+  dark = !dark;
+  localStorage.setItem("dark", dark);
+  document.body.classList.toggle("modoOscuro");
+  if (dark) {
+    btnModoOscuro.innerText = "Light";
+    return;
+  }
+  btnModoOscuro.innerText = "Dark";
+};

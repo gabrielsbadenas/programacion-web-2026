bacc390910a5a9e9e60f995afeb40a6e8bb62b05
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2026-06-11 00:08:46 +0000

    10

--- 10-6/asincronia.js
+++ 10-6/asincronia.js
@@ -1,4 +1,21 @@
-let numero = 10
-let intervalo = setInterval(()=>{
-    numero--
-},10)
+console.log("principio del programa");
+//nos permite que algo trde en ejecutarse
+setTimeout(function () {
+  console.log("ejecucion del programa");
+}, 0);
+console.log("final del programa");
+let numero1 = 1;
+let intervalo1 = setInterval(function () {
+  console.log(numero1);
+  numero1++;
+  if (numero == 20) clearInterval(intervalo1);
+}, 1000);
+let numero = 1000;
+let intervalo = setInterval(function () {
+  console.log(numero);
+  numero--;
+  if (numero == 0) {
+    console.log("perdiste");
+    clearInterval(intervalo);
+  }
+}, 1000);

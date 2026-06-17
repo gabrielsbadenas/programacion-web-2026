47548d9007da80f274d4204d6f3154cfdbd178eb
Gabriel <66344280+gabrielsbadenas@users.noreply.github.com>
2026-06-17 22:51:54 +0000

    pokemon

--- 17-6/promesas.js
+++ 17-6/promesas.js
@@ -1,11 +1,14 @@
-fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+fetch("https://pokeapi.co/api/v2/pokemon?limit=1000")
   .then(res => res.json())
   .then(data => {
     data.results.forEach(pokemon => {
-      document.getElementById("pokemon").innerHTML += `
-        <h2>${pokemon.name}</h2>
-      `;
-      console.log(pokemon)
+      fetch(pokemon.url)
+        .then(res => res.json())
+        .then(pokemonData => {
+          document.getElementById("pokemon").innerHTML += `
+            <p>Name: ${pokemon.name} - types: ${pokemonData.types.map(t => t.type.name)} - Weight: ${pokemonData.weight}</p>
+          `;
+        });
     });
   })
   .catch(error => console.error('Error:', error));
